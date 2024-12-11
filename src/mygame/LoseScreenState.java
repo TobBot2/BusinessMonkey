0cfc336eb8a7600f38fe7cc05ea08c51dc9becdf
Samuel Muzac <smuzac1@jh.edu>
2024-12-11 12:11:36 -0500

    shading implementation and small menu edits

--- src/mygame/LoseScreenState.java
+++ src/mygame/LoseScreenState.java
@@ -12,8 +12,11 @@ import com.jme3.font.BitmapText;
 import com.jme3.input.KeyInput;
 import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.KeyTrigger;
+import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
+import com.jme3.scene.shape.Quad;
 
 
 /**
@@ -36,6 +39,14 @@ public class LoseScreenState extends AbstractAppState implements ActionListener{
         super.initialize(stateManager, app);
         this.stateManager = stateManager;
         
+//        // Background
+//        Geometry background = new Geometry("Background", new Quad(app.getCamera().getWidth(), app.getCamera().getHeight()));
+//        Material bgMaterial = new Material(app.getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
+//        bgMaterial.setColor("Color", ColorRGBA.Red); // Change to any color you like
+//        background.setMaterial(bgMaterial);
+//        background.setLocalTranslation(0, 0, -1); // Position slightly behind GUI elements
+//        guiNode.attachChild(background);
+//        
         guiNode = ((Main) app).getGuiNode();
         
         BitmapFont font = stateManager.getApplication().getAssetManager().loadFont("Interface/Fonts/Default.fnt");
--- src/mygame/WinScreenState.java
+++ src/mygame/WinScreenState.java
@@ -14,8 +14,11 @@ import com.jme3.input.InputManager;
 import com.jme3.input.KeyInput;
 import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.KeyTrigger;
+import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
+import com.jme3.scene.shape.Quad;
 import mygame.Main;
 
 
@@ -41,6 +44,14 @@ public class WinScreenState extends AbstractAppState implements ActionListener{
         this.stateManager = stateManager;
         
         guiNode = ((Main) app).getGuiNode();
+        
+//        // Background
+//        Geometry background = new Geometry("Background", new Quad(app.getCamera().getWidth(), app.getCamera().getHeight()));
+//        Material bgMaterial = new Material(app.getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
+//        bgMaterial.setColor("Color", ColorRGBA.Green); // Change to any color you like
+//        background.setMaterial(bgMaterial);
+//        background.setLocalTranslation(0, 0, -1); // Position slightly behind GUI elements
+//        guiNode.attachChild(background);
         
         BitmapFont font = stateManager.getApplication().getAssetManager().loadFont("Interface/Fonts/Default.fnt");
         winText = new BitmapText(font, false);
